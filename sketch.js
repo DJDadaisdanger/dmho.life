3de2b20e49dc54fef8792850ef101a3f162c19d4
djdadaisdanger <djdadaisdanger@gmail.com>
2025-09-19 12:59:47 +0000

    Recommit

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var vehicles = [];
 var font; 
 var points;
@@ -27,34 +26,4 @@ for(var i = 0;i < points.length;i++){
   v.update();
   v.show();
 }
-=======
-  var vehicles = [];
-  var font; 
-  var points;
-function preload(){
-    font = loadFont('font.TTF')  
-}
-function setup() {
-  createCanvas(1290, 300);
-  background(0);
-  textFont(font);
-
-  
-  points = font.textToPoints('hello world!!',100,232,192);
-  for(var i = 0;i < points.length;i++){
-    var pt = points[i];
-    var vehicle = new Vehicle(pt.x,pt.y);
-    vehicles.push(vehicle);
-  }
-}
-
-function draw() {
-  background(00);
-  for(var i = 0;i < points.length;i++){
-    var v = vehicles[i];
-    v.behaviors();
-    v.update();
-    v.show();
-  }
->>>>>>> 7bc02b3fafb2f08a66d1fd4a8a68b69dc61ff9c7
 }
