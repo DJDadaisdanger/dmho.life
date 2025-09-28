8cabec124efcaf14d151a93c3600bd6a7026114f
djdadaisdanger <djdadaisdanger@gmail.com>
2025-09-28 11:59:37 +0000

    added new page

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,8 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.nodejs_20
   ];
 
   # Sets environment variables in the workspace
@@ -25,16 +22,13 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["sh", "-c", "python3 -m http.server $PORT"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
       };
     };
 
